57431178e107686aab34d4be91e05b82926c0225
Adrian Ferreres Esteller <ardiadrianadri@gmail.com>
2019-02-22 06:26:20 +0100

    Add flag to show/ hide the expand button

--- src/config-list/components/item-list.js
+++ src/config-list/components/item-list.js
@@ -23,9 +23,14 @@ export class ItemList extends Component {
     }
 
     render() {
+        let icon = null;
+        if (this.state.header) {
+            icon = (this.state.open) ? 'add' : 'remove';
+        }
+
         return (
             <div className="item-list">
-                <span className="expand material-icons mr-3" onClick={this.handleExpand.bind(this)}>{(this.state.open) ? 'add' : 'remove' }</span>
+                <span className="expand material-icons mr-3" onClick={this.handleExpand.bind(this)}>{icon}</span>
                 <span className={classNames("icon", "material-icons", " mr-5", {"must": this.state.header})}>{this.props.icon}</span>
                 <span className="title">{this.props.title}</span>
                 <span className="menu material-icons ml-auto">more_vert</span>
